0d8c70a646f0fa3cb1396ffdfaf35304010e48ef
JazminRondon <Jazmin_Rondon@outlook.com>
2022-10-04 20:43:32 -0400

    more code

---
+++ src/main/java/brocode/PackagesAndImportKeyWords.java
@@ -0,0 +1,14 @@
+package brocode;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class PackagesAndImportKeyWords {
+    public static void main(String[] args) {
+        Date date=new Date();
+        String s= new String();
+        LocalDate localDate= LocalDate.now();
+        LocalDateTime localDateTime= LocalDateTime.now();
+    }
+}
