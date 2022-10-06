fb2f22c318bc51694c614999c15d55c9d4087419
JazminRondon <Jazmin_Rondon@outlook.com>
2022-10-06 10:52:52 -0400

    coding along

--- src/main/java/amigoscode/UnderstandingMethods.java
+++ src/main/java/amigoscode/UnderstandingMethods.java
@@ -1,4 +1,23 @@
 package amigoscode;
 
+import java.util.Arrays;
+
 public class UnderstandingMethods {
+    public static void main(String[] args) {
+        char[] letters= {'A','A','B','C','D','D','D'};
+        int count=countOccurrences(letters,'D');
+        System.out.println(count);
+    }
+    public static int countOccurrences(char[] letters,char searchLetter){
+        int count=0;
+        for (char letter : letters) {
+            if(letter==searchLetter){
+             count++;
+            }
+        }
+ //       System.out.println("method countOccuraences was involked");
+//        System.out.println(Arrays.toString(letters));
+//        System.out.println(searchLetter);
+        return count;
+    }
 }
